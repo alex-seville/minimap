3ec1542451623a128e1ccad8cfa81403528e320a
alex-seville <alexander.southern@gmail.com>
2012-09-02 14:22:00 -0400

    Updated the code, fixed scrolling issues when adding new content, added comments, changed insertion of html and css into DOM

--- minimap.js
+++ minimap.js
@@ -1,100 +1,96 @@
 // minimap scroller
 
 (function(){
+  //This is the HTML and css we use for our control.
+  var sourceHTML = "<div id='sourceScroll'></div>";
+  var indicatorHTML = "<div id='scrollIndicator'></div>";
+  var sourceCSS = {
+      "width": "100px",
+      "max-height":"300px",
+      "opacity": "0.5",
+      "background-color":"#aaa",
+      "position": "absolute",
+      "right": "0",
+      "top": "0",
+      "color": "#000",
+      "overflow":"hidden",
+      "font-size": "4px",
+      "line-height":"4px",
+      "z-index": "9"
+  };
 
-  var miniMapHTML = "<div id='sourceScroll'></div><div id='scrollIndicator'></div>";
-  var miniMapCSS = "             \
-    #sourceScroll {              \
-        width: 100px;            \
-        max-height:300px;        \
-        opacity: 0.5;            \
-        background-color:#aaa;   \
-        position: absolute;      \
-        right: 0;                \
-        top: 0;                  \
-        color: #000;             \
-        overflow:hidden;         \
-        font-size: 4px;          \
-        line-height:4px;         \
-        z-index: 9;              \
-    }                            \
-                                 \
-    #scrollIndicator {           \
-      width: 100px;              \
-      height: 40px;              \
-      opacity: 0.5;              \
-      background-color:#fff;     \
-      position: absolute;        \
-      right:0;                   \
-      top:0;                     \
-      z-index: 9;                \
-    }";
-
-    var scrollMode = false;
-    var HEIGHT = "height";
-    var PX = "px";
-    var getHeightValue = function($obj){
-      return parseInt($obj.css(HEIGHT).replace(PX,""),10);
+  var indicatorCSS = {
+    "width": "100px",
+    "height": "40px",
+    "opacity": "0.5",
+    "background-color":"#fff",
+    "position": "absolute",
+    "right":"0",
+    "top":"0",
+    "z-index": "9"
+  };
+  //we start assuming that we will use fractionalScrolling
+  var scrollMode = false;
+  var HEIGHT = "height";
+  var PX = "px";
+  //We curry the scoll method because the factor and px variables
+  //don't change often.
+  function setupScrollAction(factor,px){
+    return function($show,$ss,$watch){
+      var st = $watch.scrollTop();
+      if (scrollMode){
+        $ss.scrollTop(st);
+      }
+      $show.stop().css({"marginTop": (st*factor) + px} );
     };
-    function newStyle(str){
-      var pa= document.getElementsByTagName('head')[0] ;
-      var el= document.createElement('style');
-      el.type= 'text/css';
-      el.media= 'screen';
-      if(el.styleSheet) el.styleSheet.cssText= str;// IE method
-      else el.appendChild(document.createTextNode(str));// others
-      pa.appendChild(el);
-      return el;
-    }
-    function setupScrollAction(factor,px){
-      return function($show,$ss,$watch){
-        var st = $watch.scrollTop();
-        if (scrollMode){
-          $ss.scrollTop(st);
-        }
-        $show.stop().css({"marginTop": (st*factor) + px} );
-      };
-    }
+  }
 
   /**
-* @miniMap
-* @public
-*
-* @param {function} compiledTemplate - a compiled template function (the parent template)
-* @param {object} renderObj - the Object you are passing into the template
-* @param {function} fetchTemplate - callback to fetch the partial view template
-*
-*/
+   * @miniMap
+   * @public
+   *
+   */
   this.miniMap = function(options){
     var $watch = $(options.scrollBar);
-    $watch.after(miniMapHTML);
-    newStyle(miniMapCSS);
-    var $show = $("#scrollIndicator");
-    var $ss = $("#sourceScroll");
-    
+    //create our control
+    var $ss = $(sourceHTML).css(sourceCSS);
+    var $show = $(indicatorHTML).css(indicatorCSS);
+    //insert into DOM
+    $watch.after($show);
+    $watch.after($ss);
+    //We need to modify the style to match where it was placed in the DOM.
     $ss.css("max-height",$watch.css(HEIGHT)+PX);
-    $ss.css("overflow","none");
-
     $ss.css("right",$watch.css("width"));
     $show.css("right",$watch.css("width"));
 
     var doScroll = null;
     
     function matchSource(){
-      var newContent = $(options.content).html();
-      console.log("change: "+newContent);
+      var factor, max, avail;
+      //we have to clone to new content because we need to
+      //remove any elements that might distort the scrollHeight
+      //of our source pane
+      var newContentDOM = $(options.content).clone();
+      //Here we null-ify any top or bottom settings
+      newContentDOM.find("[style*='top']").css("top","");
+      newContentDOM.find("[style*='bottom']").css("bottom","");
+      //then we take the new html
+      var newContent = newContentDOM.html();
       $ss.html(newContent);
-      scrollMode = $ss.get(0).scrollHeight > $ss.height();
-      var max =$watch[0].scrollHeight - getHeightValue($watch);
-      var avail = getHeightValue($ss) - getHeightValue($show);
-      var factor = avail/max;
+      //check if we should be fractionally scrolling (false),
+      // or normal scrolling (true)
+      var ssHeight = $ss.height();
+      scrollMode = $ss.get(0).scrollHeight > ssHeight+1;
+      
+      max =$watch[0].scrollHeight - $watch.height();
+      avail = ssHeight - $show.height();
+      factor = avail/max;
+      //mmm...curry.
       doScroll = setupScrollAction(factor,PX);
     }
 
     matchSource();
-
-    
-
+    //when we scroll, do the current scroll function
     $watch.scroll(function(){
       doScroll($show,$ss,$watch);
     });
